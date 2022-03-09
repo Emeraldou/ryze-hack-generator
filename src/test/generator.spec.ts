1d505e80d9863a471258c17b754bd846909ab6e4
FabienLegouge <fabien.legouge@dimobile.fr>
2022-03-09 22:48:48 +0100

    1.0.0

--- src/test/generator.spec.ts
+++ src/test/generator.spec.ts
@@ -1,12 +1,5 @@
 import test from 'ava';
 
-import RyzeHackGenerator from '../index';
-
 test('logs', t => {
-  t.log(RyzeHackGenerator.generate({
-    wordPool: (pool: Array<string>) => {
-      pool.push("TEST");
-      return pool;
-    },
-  }));
+  t.pass();
 });
